4ae0efe9e00d36e948895e3ee41f7637f7e99e8d
Катрин Райлян <k.shalamowa@gmail.com>
2018-10-07 23:17:18 +0300

    refactor: updated according to ES6

--- src/index.js
+++ src/index.js
@@ -1,56 +1,48 @@
 module.exports = function solveSudoku(matrix) {
+    const solvedSukodu = [], len = matrix.length;
 
-    var matrixNew = [];
-
-        for (i=0; i<matrix.length; i++){
-            for (j=0; j<matrix.length;j++){
-                matrixNew.push(matrix[i][j]);
-            }
-
+    for (horizontal = 0; horizontal < len; horizontal++) {
+      for (vertical = 0; vertical < len; vertical++) {
+        solvedSukodu.push(matrix[horizontal][vertical]);
+      }
+    }
+  
+    function checker(number, row, column) {
+      for (let i = 0; i < 9; i++) {
+        let third = ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) * 9 + (Math.floor(column / 3) * 3) + (i % 3);
+        if (number == solvedSukodu[(row * 9) + i] || number == solvedSukodu[column + (i * 9)] || number == solvedSukodu[third]) {
+          return false;
         }
-
-
-    function check_candidate(num, row, col) {
-        for (var i = 0; i < 9; i++) {
-            var b_index = ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) * 9 + (Math.floor(col / 3) * 3) + (i % 3);
-            if (num == matrixNew[(row * 9) + i] ||
-                num == matrixNew[col + (i * 9)] ||
-                num == matrixNew[b_index]) {
-                return false;
-            }
-        }
+      }
+      return true;
+    }
+  
+    function getter(index) {
+      if (index >= solvedSukodu.length) {
         return true;
-    }
-
-    function get_candidate(index) {
-        if (index >= matrixNew.length) {
+      } else if (solvedSukodu[index] != 0) {
+        return getter(index + 1);
+      }
+      
+      for (let i = 1; i <= 9; i++) {
+        if (checker(i, Math.floor(index / 9), index % 9)) {
+          solvedSukodu[index] = i;
+          if (getter(index + 1)) {
             return true;
-        } else if (matrixNew[index] != 0) {
-            return get_candidate(index + 1);
+          }
         }
-
-        for (var i = 1; i <= 9; i++) {
-            if (check_candidate(i, Math.floor(index / 9), index % 9)) {
-                matrixNew[index] = i;
-                if (get_candidate(index + 1)) {
-                    return true;
-                }
-            }
-        }
-
-        matrixNew[index] = 0;
-        return false;
+      }
+      solvedSukodu[index] = 0;
+      return false;
     }
-
-    function chunk_in_groups(arr) {
-        var result = [];
-        for (var i = 0; i < arr.length; i += 9) {
-            result.push(arr.slice(i, i + 9));
-        }
-        return result;
+  
+    function cluster(array) {
+      const result = [];
+      for (let i = 0; i < arrray.length; i += 9) {
+        result.push(array.slice(i, i + 9));
+      }
+      return result;
     }
-
-    return !get_candidate(0) ? 'No solution found.' :
-        chunk_in_groups(matrixNew);
-
-}
+    return !getter(0) ? 'These are not the numbers you-re looking for...' :
+      cluster(solvedSukodu);
+  }
